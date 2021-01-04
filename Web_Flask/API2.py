6d62919cbe5135a02748eb67425a5de4a0974be7
ThibaultLeconte <thibault.leconte@cpe.fr>
2021-01-04 13:51:01 +0100

    lucas le bg

---
+++ Web_Flask/API2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from flask_ngrok import run_with_ngrok
+from flask import Flask
+app = Flask(__name__)
+run_with_ngrok(app)   #starts ngrok when the app is run
+@app.route("/")
+def home():
+    return "<h1>Running Flask on Google Colab!</h1>"
+  
+app.run()
